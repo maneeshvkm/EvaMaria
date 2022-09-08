cb2677ce4184d6d57bb4ed176c7d1c2bd58d3319
maneeshvkm <108318244+maneeshvkm@users.noreply.github.com>
2022-09-08 07:32:56 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -244,6 +244,7 @@ async def start(client, message):
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
+        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('JOIN', url='https://t.me/HDMLinkz') ] ] ),
         protect_content=True if pre == 'filep' else False,
         )
                     
