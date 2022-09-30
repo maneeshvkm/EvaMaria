3461e204a5f7129f566f5d267809d1a0993a128a
maneeshvkm <108318244+maneeshvkm@users.noreply.github.com>
2022-09-30 22:47:54 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -244,7 +244,7 @@ async def start(client, message):
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
-        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('JOIN', url='https://t.me/HDMLinkz') ] ] ),
+        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true) ] ] ),
         protect_content=True if pre == 'filep' else False,
         )
                     
